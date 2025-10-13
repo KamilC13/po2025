c4ff1f66da1cfd260343c5e0344fd5750a37d0a9
KamilC13 <kciask@student.agh.edu.pl>
2025-10-14 01:00:02 +0200

    commit nowy

--- Lab2/src/Losowanie.java
+++ Lab2/src/Losowanie.java
@@ -2,24 +2,34 @@ import java.util.*;
 
 public class Losowanie {
     Random rand = new Random();
-    ArrayList lista = new ArrayList<>();
+    ArrayList lista = new ArrayList();
     ArrayList listatypy = new ArrayList();
     int zgodne = 0;
 
-    public void typy(){
+    public ArrayList typy(){
         Scanner sc = new Scanner(System.in);
-        for(int i = 0; i <= 5; i++){
-            int wybor = Integer.parseInt(sc.next());
-            listatypy.add(wybor);
+        while(listatypy.size()<6){
+            int n = sc.nextInt();
+            if(!listatypy.contains(n) && n<=49){
+                listatypy.add(n);
+            }
+            else {
+                System.out.println("Wypisujesz glupoty!");
+            }
         }
-        System.out.println("Wybrane typy: " + listatypy);
+        return listatypy;
     }
-    public void adding() {
-        for (int i = 0; i <= 5; i++) {
-            lista.add(rand.nextInt(49) + 1);
+    public ArrayList adding() {
+        while(lista.size() < 6){
+            int num = rand.nextInt(49)+1;
+            if(!lista.contains(num)){
+                lista.add(num);
+            }
         }
+        return lista;
     }
     public int spr() {
+        zgodne = 0;
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j <= 5; j++) {
                 if (lista.get(i).equals(listatypy.get(j))) {
@@ -31,26 +41,30 @@ public class Losowanie {
     }
     public void czyszczenie(){
         lista.clear();
-        zgodne = 0;
     }
-
 
 
     public static void main(String[] args) {
         Losowanie proba = new Losowanie();
         int liczba = 0;
-        proba.typy();
+        int trafione = 0;
 
-        while(proba.zgodne != 6){
+        proba.typy();
+        while(trafione != 6) {
             proba.adding();
-            proba.spr();
+            trafione = proba.spr();
             liczba++;
-            proba.czyszczenie();
+            if(trafione != 6) {
+                proba.czyszczenie();
+            }
         }
-        System.out.println("Liczba przejsc: " + liczba);
-        System.out.println("Trafionych jest: " + proba.zgodne);
-        }
+        System.out.println("typy " + proba.listatypy);
+        System.out.println("wylosowane " + proba.lista);
+        System.out.println("zgodna ilosc " + proba.zgodne);
+        System.out.println("ilosc losowan " + liczba);
+
     }
+}
 
 
 
